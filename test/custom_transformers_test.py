1ff2400ea3dd07a12c48f3f93337fabbb2f16123
Carlos Eduardo Tussi <38245497+CarlosTussi@users.noreply.github.com>
2025-04-14 23:23:58 +0200

    Adding test ContainWordsTransformer and ClusterGeolocationTransformer

--- test/custom_transformers_test.py
+++ test/custom_transformers_test.py
@@ -1,7 +1,7 @@
 '''
     How to run:
     1) From rio_listing main folder
-    2) python -m pytest .\test\
+    2) python -m pytest -v .\test\
 
 '''
 
@@ -11,36 +11,78 @@ from literals import *
 import pandas as pd
 import numpy as np
 
+
+
+'''
+    Testing: ClusterGeolocationTransformer
+'''
+def test_ClusterGeolocationTransformer():
+    X_geolocation
+    geocluster1 = ClusterGeolocationTransformer()
+    geocluster1.fit(X_geolocation)
+    X_mod = geocluster1.transform(X_geolocation)
+    X_mod_cols = [geo_col for geo_col in X_mod.columns if "geo_cluster" in geo_col]
+
+    # TESTING: geo_cluster columns were created
+    assert(len(X_mod_cols) > 0 )
+    # TESTING: Absence of missing values in the columns generated
+    assert(X_mod[X_mod_cols].isna().sum().all() == 0)
+    # TESTING: Transformed dataset is a Pandas DataFrame 
+    assert(isinstance(X_mod, pd.DataFrame))
+
+
+
+'''
+    Testing: ContainWordsTransformer
+'''
+def test_ContainWordsTransformer():
+    NEW_FT_NAME = "contains_lux_description"
+    TARGET = "description"
+    WORDS = ["luxurious","fancy", "sophistication", "rooftop"]
+    containwords = ContainWordsTransformer(new_feature_name = NEW_FT_NAME,
+                                            corpus_target= TARGET,
+                                            words = WORDS
+    ) 
+    containwords.fit(X_lux)
+    X_mod = containwords.transform(X_lux)
+
+    # TESTING: New feature was added to the dataset
+    assert(NEW_FT_NAME in X_mod.columns)
+    # TESTING: Words were correctly detected in the corpus
+    assert(X_mod[NEW_FT_NAME].equals(X_processed_lux))
+
+
+
 '''
     Testing: DropNasTransformer
 '''
 def test_DropNasTransformer():
     
-    ########################
-    # Remove from 1 column #
-    ########################
     # Removing missing values from columns A
     drop_nas1 = DropNasTransformer(["A"])
     drop_nas1.fit(X_missing)
-    # No empty columns after transformation
+    # TESTING: No NAs removed from single column
     assert pd.isnull(drop_nas1.transform(X_missing)["A"]).sum() == 0
 
-    ################################
-    # Remove from multiple columns #
-    ################################
+
     # Removing missing values from columns A and C
     drop_nas2 = DropNasTransformer(["A", "C"])
     drop_nas2.fit(X_missing)
     # No empty columns after transformation
-    assert pd.isnull(drop_nas1.transform(X_missing)[["A","C"]]).sum().all() == 0
+    X_mod = drop_nas1.transform(X_missing)
+    # TESTING: NAs removed from multiple columns
+    assert pd.isnull(X_mod[["A","C"]]).sum().all() == 0
 
-    ######################################
+
     # Columns without any missing values #
-    ######################################
-    rop_nas2 = DropNasTransformer(["A", "C"])
-    drop_nas2.fit(X_missing)
-    # No empty columns after transformation
-    assert pd.isnull(drop_nas1.transform(X_missing)["D"]).sum() == 0
+    drop_nas3 = DropNasTransformer(["A", "C"])
+    drop_nas3.fit(X_missing)
+    X_mod = drop_nas3.transform(X_missing)
+    # TESTING: Column has no missing values before transform
+    assert pd.isnull(X_mod["D"]).sum() == 0
+    
+    # TESTING: Transformed dataset is a Pandas DataFrame 
+    assert(isinstance(X_mod, pd.DataFrame))
 
 
 '''
@@ -48,14 +90,13 @@ def test_DropNasTransformer():
 '''
 
 def test_PreprocessCorpus():
-    X_corpus
 
-    ################################
-    # Removing unwanted characters #
-    ################################
     preprocess_corpus1 = PreprocessCorpus("description")
     preprocess_corpus1.fit(X_corpus)
-    assert(preprocess_corpus1.transform(X_corpus).equals(X_processed_corpus))
+    X_mod = preprocess_corpus1.transform(X_corpus)
 
+    # TESTING: Unwanted characters were correctly removed
+    assert(X_mod.equals(X_processed_corpus))
+    # TESTING Transformed dataset is a Pandas DataFrame 
+    assert(isinstance(X_mod, pd.DataFrame))
 
-    
--- test/literals.py
+++ test/literals.py
@@ -31,3 +31,31 @@ X_processed_corpus = pd.DataFrame({
         "amazing ipanema beach min bakery"
     ]
 })
+
+# lux words being considered ["luxurious","fancy", "sophistication", "rooftop"]
+X_lux =  pd.DataFrame({ 
+    'description': [
+        "newly luxurious decorated fancy penthouse copacabana beach incredible seaviews balcony jacuzzi hottub",
+        "small but amazing house touch sophistication square meters",
+        "rooftop very close ocean",
+        "check my accommodation website more details http www myhouse rio janeiro com",
+        "please contact directly owner after paying",
+        "amazing ipanema beach min bakery"
+    ]
+})
+# 1: if lux word present 
+# 0: if lux word not present
+X_processed_lux = pd.Series([1,1,1,0,0,0]) 
+
+X_geolocation = pd.DataFrame({
+    "latitude": [
+        -22.9701, -22.9685, -22.9723, -22.9667, -22.9738,  
+        -22.9842, -22.9887, -22.9815, -22.9871, -22.9856, 
+        -23.0123, -23.0089, -23.0156, -23.0044, -23.0182   
+    ],
+    "longitude": [
+        -43.1856, -43.1821, -43.1883, -43.1809, -43.1867,  
+        -43.2001, -43.1987, -43.2023, -43.1965, -43.2042,  
+        -43.3601, -43.3587, -43.3625, -43.3559, -43.3642
+    ]
+})
